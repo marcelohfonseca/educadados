f4bc5829da861cb1144a5bc474e45ad446288728
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2022-06-04 17:13:14 -0300

    Inclusão de loop para download de vários arquivos

--- src/download_files.py
+++ src/download_files.py
@@ -1,16 +1,26 @@
+from datetime import date
 import requests
+import os
 
 def download(url, directory):
 
-    URL = 'https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_2023.zip'
-    FOLDER = '../data'
-
-    file = requests.get(URL)
+    file = requests.get(url)
     if file.status_code == requests.codes.OK:
-        with open(FOLDER, 'wb') as new_file:
+        with open(directory, 'wb') as new_file:
             new_file.write(file.content)
         print('Download do arquivo realizado com sucesso.')
     else:
+        print('Este arquivo não existe')
         print(file.raise_for_status())
 
 
+if __name__ == "__main__":
+    URL = 'https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_{}.zip'
+    FOLDER = '../data'
+
+    last_year = date.today().year
+    first_year = last_year - 10
+
+    for year in range(first_year, last_year):
+        file_name = os.path.join(FOLDER, f'microdados_censo_da_educacao_superior_{year}.zip')
+        download(URL.format(year), file_name)
