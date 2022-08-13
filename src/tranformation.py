a07267d2b82561c47505219d094a3f6c7e246c93
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2022-08-13 12:58:52 -0300

    Removido trecho de código duplicado

--- src/tranformation.py
+++ src/tranformation.py
@@ -93,48 +93,6 @@ class DataBase:
             raise Exception('O tipo de dados passado não está correto ou não foi informado o nome da tabela!')
         return print('Query executada com sucesso!') if True else print('Erro ao executar query!')
 
-# create table
-with open('../scripts/create_table_ft_oferta_curso.sql', 'r') as file:
-    create_table_str = str(file.read())
-
-# converter csv em dataframe
-folder = '../data/censo-educacao-superior/'
-files = os.listdir(folder)
-df = pd.DataFrame()
-
-for file in files:
-    with zip.ZipFile(folder + file, 'r') as zip_file:
-        for name in zip_file.namelist():
-            if name.find('MICRODADOS_CADASTRO_CURSOS_2020') > 0:
-                with zip_file.open(name) as csv_file:
-                    df_tmp = pd.read_csv(csv_file, encoding='latin1', sep=';')
-                    df = df.append(df_tmp)
-            else:
-                pass
-
-columns = ['NU_ANO_CENSO','CO_IES','CO_MUNICIPIO','CO_CINE_ROTULO',
-            'NO_CINE_ROTULO','TP_GRAU_ACADEMICO','TP_MODALIDADE_ENSINO',
-            'TP_NIVEL_ACADEMICO','QT_VG_TOTAL','QT_INSCRITO_TOTAL',
-            'QT_ING','QT_MAT','QT_CONC']
-
-ft_oferta_curso = df[columns]
-name_columns = {'NU_ANO_CENSO': 'NR_ANO',
-                'CO_IES': 'CD_IES',
-                'CO_MUNICIPIO': 'CD_MUNICIPIO',
-                'CO_CINE_ROTULO': 'CD_CURSO',
-                'NO_CINE_ROTULO': 'NM_CURSO',
-                'TP_GRAU_ACADEMICO': 'CD_GRAU_ACADEMICO',
-                'TP_MODALIDADE_ENSINO': 'CD_MODALIDADE_ENSINO',
-                'TP_NIVEL_ACADEMICO': 'CD_NIVEL_ACADEMICO',
-                'QT_VG_TOTAL': 'QT_OFERTA_TOTAL',
-                'QT_INSCRITO_TOTAL': 'QT_INSCRITO_TOTAL',
-                'QT_ING': 'QT_INGRESSANTE_TOTAL',
-                'QT_MAT': 'QT_MATRICULA_TOTAL',
-                'QT_CONC': 'QT_CONCLUINTE_TOTAL'}
-
-ft_oferta_curso.rename(columns=name_columns, inplace=True)
-ft_oferta_curso.fillna(0, inplace=True)
-
 
 if __name__ == "__main__":
 
