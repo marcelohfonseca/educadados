d86de3bc08a1e2e9de52a31a6f851207197a3ece
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2022-08-13 11:46:48 -0300

    Criado funções para conexão e inserção dos dados

--- src/tranformation.py
+++ src/tranformation.py
@@ -9,44 +9,7 @@ from sqlalchemy_utils import database_exists, create_database
 from dotenv import load_dotenv
 
 
-folder = '../data/censo-educacao-superior/'
-
-files = os.listdir(folder)
-df = pd.DataFrame()
-
-for file in files:
-    with zip.ZipFile(folder + file, 'r') as zip_file:
-        for name in zip_file.namelist():
-            if name.find('MICRODADOS_CADASTRO_CURSOS_2020') > 0:
-                with zip_file.open(name) as csv_file:
-                    df_tmp = pd.read_csv(csv_file, encoding='latin1', sep=';')
-                    df = df.append(df_tmp)
-            else:
-                pass         
-
-columns = ['NU_ANO_CENSO','CO_IES','CO_MUNICIPIO','CO_CINE_ROTULO','NO_CINE_ROTULO','TP_GRAU_ACADEMICO','TP_MODALIDADE_ENSINO','TP_NIVEL_ACADEMICO',
-           'QT_VG_TOTAL','QT_INSCRITO_TOTAL','QT_ING','QT_MAT','QT_CONC']
-
-ft_oferta_curso = df[columns]
-
-name_columns = {'NU_ANO_CENSO': 'NR_ANO',
-                'CO_IES': 'CD_IES',
-                'CO_MUNICIPIO': 'CD_MUNICIPIO',
-                'CO_CINE_ROTULO': 'CD_CURSO',
-                'NO_CINE_ROTULO': 'NM_CURSO',
-                'TP_GRAU_ACADEMICO': 'CD_GRAU_ACADEMICO',
-                'TP_MODALIDADE_ENSINO': 'CD_MODALIDADE_ENSINO',
-                'TP_NIVEL_ACADEMICO': 'CD_NIVEL_ACADEMICO',
-                'QT_VG_TOTAL': 'QT_OFERTA_TOTAL',
-                'QT_INSCRITO_TOTAL': 'QT_INSCRITO_TOTAL',
-                'QT_ING': 'QT_INGRESSANTE_TOTAL',
-                'QT_MAT': 'QT_MATRICULA_TOTAL',
-                'QT_CONC': 'QT_CONCLUINTE_TOTAL'}
-
-ft_oferta_curso.rename(columns=name_columns, inplace=True)
-ft_oferta_curso.fillna(0, inplace=True)
-
-# configurações de banco de dados mysql
+# configurações de banco de dados mysql    
 load_dotenv()
 DB_USER=os.getenv('DB_USER')
 DB_PASSWORD=os.getenv('DB_PASSWORD')
@@ -54,37 +17,130 @@ DB_HOST=os.getenv('DB_HOST')
 DB_PORT=os.getenv('DB_PORT')
 DBNAME = 'EDUCADADOS'
 
-url_db = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DBNAME}'
 
-engine = create_engine(url_db)
-if not database_exists(engine.url):
-    create_database(engine.url)
-
-engine.connect()
-
-# criando a tabela FT_OFERTA_CURSO
-engine.execute(
+class DataBase:
+    """_summary_
     """
-    CREATE TABLE IF NOT EXISTS FT_OFERTA_CURSO (
-        `ID` INT KEY AUTO_INCREMENT,
-        `NR_ANO` INT NULL,
-        `CD_IES` INT NULL,
-        `CD_MUNICIPIO` INT NULL,
-        `CD_CURSO` VARCHAR(80) NULL,
-        `CD_GRAU_ACADEMICO` INT NULL,
-        `CD_MODALIDADE_ENSINO` INT NULL,
-        `CD_NIVEL_ACADEMICO` INT NULL,
-        `QT_OFERTA_TOTAL` INT NULL,
-        `QT_INSCRITO_TOTAL` INT NULL,
-        `QT_INGRESSANTE_TOTAL` INT NULL,
-        `QT_MATRICULA_TOTAL` INT NULL,
-        `QT_CONCLUINTE_TOTAL` INT NULL
-    );
-    """
-)
+    def __init__(self, user: str, password: str, host: str, port: int, database: str) -> None:
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
 
-# insert dos dados na tabela FT_OFERTA_CURSO
-metadata = schema.MetaData(bind=engine)
-table = Table('FT_OFERTA_CURSO', metadata, autoload=True)
-insert_rows_dict = ft_oferta_curso.to_dict(orient='records')
-engine.execute(table.insert(), insert_rows_dict)
+
+    def execute_query(self, query_or_dataframe: str, table_name: str = None) -> object:
+        """Executa uma query no banco de dados, para CREATE, UPDATE, SELECT e DELETE.
+
+        Args:
+            query (str): Query a ser executada
+            engine (object): Objeto de conexão com o banco de dados
+
+        Returns:
+            None: Não retorna nenhum valor, apenas o resultado da query
+        """
+
+        def connect_db() -> object:
+            """Faz a conexão com o banco de dados MySQL.
+
+            Args:
+                user (str): usuário de conexão com o banco de dados
+                password (str): senha de conexão com o banco de dados
+                host (str): endereço do banco de dados
+                port (int): porta de conexão com o banco de dados
+                database (str): nome do banco de dados
+
+            Returns:
+                object: retorna o objeto de conexão com o banco de dados
+            """
+            url_db = f'mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}'
+            
+            try:        
+                engine = create_engine(url_db)
+                if not database_exists(engine.url):
+                    create_database(engine.url)
+                print('Conexão com banco de dados estabelecida com sucesso!')
+            except Exception as error:
+                print('Erro ao conectar com o banco de dados!')
+                print(error)
+            return engine if engine else None
+    
+        def insert_dataframe(dataframe: object, table_name: str, engine: object) -> None:
+            """Insere dados no banco de dados.
+
+            Args:
+                dataframe (object): Dataframe a ser inserido no banco de dados
+                table_name (str): Nome da tabela no banco de dados
+                engine (object): Objeto de conexão com o banco de dados
+
+            Returns:
+                None: Não retorna nenhum valor, apenas o resultado da query
+            """
+            # insert dos dados na tabela FT_OFERTA_CURSO
+            metadata = schema.MetaData(bind=engine)
+            table = Table(table_name, metadata, autoload=True)
+            insert_rows_dict = dataframe.to_dict(orient='records')
+            engine.execute(table.insert(), insert_rows_dict)
+            return None
+
+        engine = connect_db()
+        engine.connect()
+        if table_name is None:
+            engine.execute(query_or_dataframe)
+        else:
+            insert_dataframe(query_or_dataframe, table_name, engine)
+        return print('Query executada com sucesso!') if True else print('Erro ao executar query!')
+
+
+if __name__ == "__main__":
+
+    # criar objeto de conexão com o banco de dados
+    new_data = DataBase(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DBNAME)
+    
+    # create table
+    with open('../scripts/create_table_ft_oferta_curso.sql', 'r') as file:
+        create_table_str = str(file.read())
+        new_data.execute_query(create_table_str)
+    
+    # converter csv em dataframe
+    folder = '../data/censo-educacao-superior/'
+    files = os.listdir(folder)
+    df = pd.DataFrame()
+
+    for file in files:
+        with zip.ZipFile(folder + file, 'r') as zip_file:
+            for name in zip_file.namelist():
+                if name.find('MICRODADOS_CADASTRO_CURSOS_2020') > 0:
+                    with zip_file.open(name) as csv_file:
+                        df_tmp = pd.read_csv(csv_file, encoding='latin1', sep=';')
+                        df = df.append(df_tmp)
+                else:
+                    pass
+
+    columns = ['NU_ANO_CENSO','CO_IES','CO_MUNICIPIO','CO_CINE_ROTULO',
+               'NO_CINE_ROTULO','TP_GRAU_ACADEMICO','TP_MODALIDADE_ENSINO',
+               'TP_NIVEL_ACADEMICO','QT_VG_TOTAL','QT_INSCRITO_TOTAL',
+               'QT_ING','QT_MAT','QT_CONC']
+    
+    ft_oferta_curso = df[columns]
+    name_columns = {'NU_ANO_CENSO': 'NR_ANO',
+                    'CO_IES': 'CD_IES',
+                    'CO_MUNICIPIO': 'CD_MUNICIPIO',
+                    'CO_CINE_ROTULO': 'CD_CURSO',
+                    'NO_CINE_ROTULO': 'NM_CURSO',
+                    'TP_GRAU_ACADEMICO': 'CD_GRAU_ACADEMICO',
+                    'TP_MODALIDADE_ENSINO': 'CD_MODALIDADE_ENSINO',
+                    'TP_NIVEL_ACADEMICO': 'CD_NIVEL_ACADEMICO',
+                    'QT_VG_TOTAL': 'QT_OFERTA_TOTAL',
+                    'QT_INSCRITO_TOTAL': 'QT_INSCRITO_TOTAL',
+                    'QT_ING': 'QT_INGRESSANTE_TOTAL',
+                    'QT_MAT': 'QT_MATRICULA_TOTAL',
+                    'QT_CONC': 'QT_CONCLUINTE_TOTAL'}
+    
+    ft_oferta_curso.rename(columns=name_columns, inplace=True)
+    ft_oferta_curso.fillna(0, inplace=True)
+
+    new_data.execute_query(ft_oferta_curso, 'FT_OFERTA_CURSO')
+
+    # limpa a tabela antes de inserir os dados
+    # engine.execute('DELETE FROM `EDUCADADOS`.`FT_OFERTA_CURSO` WHERE NR_ANO = 2020;')
