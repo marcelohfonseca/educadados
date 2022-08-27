bfb7d2f094e48f71d2e503022f05dc6c1d55ca2e
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2022-08-27 09:40:12 -0300

    Reorganização do fluxo de inserção dos dados

--- src/tranformation.py
+++ src/tranformation.py
@@ -98,51 +98,39 @@ if __name__ == "__main__":
 
     # criar objeto de conexão com o banco de dados
     new_data = DataBase(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DBNAME)
-    
+
     # create table
     with open('../scripts/create_table_ft_oferta_curso.sql', 'r') as file:
         create_table_str = str(file.read())
         new_data.execute_query(create_table_str)
-    
-    # converter csv em dataframe
-    folder = '../data/censo-educacao-superior/'
-    files = os.listdir(folder)
-    df = pd.DataFrame()
 
-    for file in files:
-        with zip.ZipFile(folder + file, 'r') as zip_file:
-            for name in zip_file.namelist():
-                if name.find('MICRODADOS_CADASTRO_CURSOS_2020') > 0:
-                    with zip_file.open(name) as csv_file:
-                        df_tmp = pd.read_csv(csv_file, encoding='latin1', sep=';')
-                        df = df.append(df_tmp)
-                else:
-                    pass
-
-    columns = ['NU_ANO_CENSO','CO_IES','CO_MUNICIPIO','CO_CINE_ROTULO',
-               'NO_CINE_ROTULO','TP_GRAU_ACADEMICO','TP_MODALIDADE_ENSINO',
-               'TP_NIVEL_ACADEMICO','QT_VG_TOTAL','QT_INSCRITO_TOTAL',
-               'QT_ING','QT_MAT','QT_CONC']
-    
-    ft_oferta_curso = df[columns]
     name_columns = {'NU_ANO_CENSO': 'NR_ANO',
                     'CO_IES': 'CD_IES',
                     'CO_MUNICIPIO': 'CD_MUNICIPIO',
                     'CO_CINE_ROTULO': 'CD_CURSO',
-                    'NO_CINE_ROTULO': 'NM_CURSO',
-                    'TP_GRAU_ACADEMICO': 'CD_GRAU_ACADEMICO',
-                    'TP_MODALIDADE_ENSINO': 'CD_MODALIDADE_ENSINO',
-                    'TP_NIVEL_ACADEMICO': 'CD_NIVEL_ACADEMICO',
                     'QT_VG_TOTAL': 'QT_OFERTA_TOTAL',
                     'QT_INSCRITO_TOTAL': 'QT_INSCRITO_TOTAL',
                     'QT_ING': 'QT_INGRESSANTE_TOTAL',
                     'QT_MAT': 'QT_MATRICULA_TOTAL',
                     'QT_CONC': 'QT_CONCLUINTE_TOTAL'}
-    
-    ft_oferta_curso.rename(columns=name_columns, inplace=True)
-    ft_oferta_curso.fillna(0, inplace=True)
 
-    new_data.execute_query(ft_oferta_curso, 'FT_OFERTA_CURSO')
+    columns = list(name_columns.keys())
 
-    # limpa a tabela antes de inserir os dados
-    # engine.execute('DELETE FROM `EDUCADADOS`.`FT_OFERTA_CURSO` WHERE NR_ANO = 2020;')
+    # converter csv em dataframe
+    folder = '../data/censo-educacao-superior/'
+    files = os.listdir(folder)
+
+    for file in files:
+        with zip.ZipFile(folder + file, 'r') as zip_file:
+            for name in zip_file.namelist():
+                if name.find('MICRODADOS_CADASTRO_CURSOS_') > 0:
+                    with zip_file.open(name) as csv_file:
+                        df_tmp = pd.read_csv(csv_file, encoding='latin1', sep=';')
+                    ft_oferta_curso = df_tmp[columns]
+                    ft_oferta_curso = ft_oferta_curso.rename(columns=name_columns)
+                    ft_oferta_curso.fillna(0, inplace=True)
+                    year = name[-8:-4]
+                    new_data.execute_query(f'DELETE FROM `EDUCADADOS`.`FT_OFERTA_CURSO` WHERE NR_ANO = {year};')
+                    new_data.execute_query(ft_oferta_curso, 'FT_OFERTA_CURSO')
+                else:
+                    pass
